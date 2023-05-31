703a2c4016a0cbc26645629ebb2abc5e17b92977
scuser <scuser@scusers-Mac-1564.local>
2023-05-31 10:16:28 -0600

    added gameover sound

--- src/scenes/gameover.ts
+++ src/scenes/gameover.ts
@@ -2,6 +2,8 @@ import Phaser from "phaser";
 import { sharedInstance as events } from "../helpers/eventCenter";
 
 export default class GameOver extends Phaser.Scene {
+    private gameOverSound!: Phaser.Sound.BaseSound;
+
     constructor() {
         super('gameover');
     }
@@ -10,14 +12,17 @@ export default class GameOver extends Phaser.Scene {
     }
 
     preload(){
-
+        this.load.audio("gameover", ["assets/sounds/gameover.mp3"]);
     }
 
     create(){
+        this.gameOverSound = this.sound.add("gameover");
+
         events.on('gameover', () => {
             this.add.text(600, 480, 'Game Over!', {
                 fontSize: '80px', color: 'white'
             });
+            this.gameOverSound.play()
         });
     }
 
